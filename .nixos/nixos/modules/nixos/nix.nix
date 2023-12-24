495198585ede7aaface104d4a9215ea6b14208f3
weriomat <eliasaengel@gmail.com>
2023-12-24 19:07:48 +0100

    chore: added rust overlay

--- .nixos/nixos/modules/nixos/nix.nix
+++ .nixos/nixos/modules/nixos/nix.nix
@@ -30,6 +30,7 @@
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
+      inputs.rust-overlay.overlays.default
 
       # Or define it inline, for example:
       # (final: prev: {
