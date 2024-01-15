d016bc6b3775c01ce48de65f9b6c81fecd4e46cf
weriomat <eliasaengel@gmail.com>
2024-01-15 19:25:56 +0100

    chore: added vi mode to zsh, need fixes tho

--- .nixos/nixos/modules/home-manager/terminal/zsh.nix
+++ .nixos/nixos/modules/home-manager/terminal/zsh.nix
@@ -5,10 +5,38 @@
     enableCompletion = true;
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
+    # TODO: fix vi keysbindings
     envExtra = ''
       # Enable Ctrl+arrow key bindings for word jumping
       bindkey '^[[1;5C' forward-word     # Ctrl+right arrow
       bindkey '^[[1;5D' backward-word    # Ctrl+left arrow
+
+      bindkey -v
+      export KEYTIMEOUT=1
+
+      # vi 
+      bindkey -M menuselect 'h' vi-backward-char
+      bindkey -M menuselect 'k' vi-up-line-or-history
+      bindkey -M menuselect 'l' vi-forward-char
+      bindkey -M menuselect 'j' vi-down-line-or-history
+
+      # change cursor shape for different vi modes
+      function zle-keymap-select() {
+        case $KEYMAP in 
+          vimcd) echo -ne '\e[1 q';; # block
+          viins|main) echo -ne '\e[5 q';; # beam
+        esac
+      }
+      zle -N zle-keymap-select
+      zle-line-init(){
+        zle -K viins # init vi ins as keymap
+        echo -ne "\e[5 q"
+      }
+      zle -N zle-line-init
+      echo -ne '\e[5 q' # use beam at startup
+      preexec(){
+        echo -ne '\e[5 q' ; # use beam for each new prompt
+      }
     '';
 
     shellAliases = {
@@ -37,8 +65,10 @@
         "sudo nixos-rebuild test --flake /home/marts/.nixos/nixos#default";
       update =
         "sudo nixos-rebuild switch --flake /home/marts/.nixos/nixos#default";
-      updatelap = "sudo nixos-rebuild switch --flake /home/marts/.nixos/nixos#laptop";
-      rebuildlap = "sudo nixos-rebuild switch --flake /home/marts/.nixos/nixos#laptop";
+      updatelap =
+        "sudo nixos-rebuild switch --flake /home/marts/.nixos/nixos#laptop";
+      rebuildlap =
+        "sudo nixos-rebuild switch --flake /home/marts/.nixos/nixos#laptop";
       rebuild =
         "sudo nixos-rebuild switch --flake /home/marts/.nixos/nixos#default";
 
