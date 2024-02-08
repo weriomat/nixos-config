8d41f0cae16dc66fd4f035cdb6dd0fa4fcbfcb26
weriomat <eliasaengel@gmail.com>
2024-02-08 21:00:34 +0100

    chore: attrset hm optimised in darwina

--- .nixos/nixos/hosts/darwina/default.nix
+++ .nixos/nixos/hosts/darwina/default.nix
@@ -1,22 +1,24 @@
 # Inspired by https://github.com/nmasur/dotfiles/blob/c53f1470ee04890f461796ba0d14cce393f2b5c3/hosts/lookingglass/default.nix
-{inputs, ...}:
+{ inputs, ... }:
 with inputs;
-  nix-darwin.lib.darwinSystem {
-    specialArgs = {inherit inputs;};
-    modules = [
-      ../../modules/darwin
-      # TODO: just enable darwin module
-      mac-app-util.darwinModules.default
-      home-manager.darwinModules.home-manager
-      {
-        home-manager.extraSpecialArgs = {inherit inputs;};
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.eliasengel.imports = [
+nix-darwin.lib.darwinSystem {
+  specialArgs = { inherit inputs; };
+  modules = [
+    ../../modules/darwin
+    # TODO: just enable darwin module
+    mac-app-util.darwinModules.default
+    home-manager.darwinModules.home-manager
+    {
+      home-manager = {
+        extraSpecialArgs = { inherit inputs; };
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.eliasengel.imports = [
           ../../modules/home-manager/darwin
           ../../modules/home-manager/common
           mac-app-util.homeManagerModules.default
         ];
-      }
-    ];
-  }
+      };
+    }
+  ];
+}
