69b9c61ea3c3fe19107452c5e1d3de1b7108643d
weriomat <eliasaengel@gmail.com>
2024-02-08 22:32:04 +0100

    chore: kitty nix-colors color-theme -> may break dependent on color scheme

--- .nixos/nixos/modules/home-manager/common/apps/kitty/default.nix
+++ .nixos/nixos/modules/home-manager/common/apps/kitty/default.nix
@@ -1,12 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   programs.kitty = {
     enable = true;
-    # TODO: fix use overlay for macos
     package =
       if pkgs.stdenv.isDarwin
       then pkgs.kitty
       else pkgs.unstable.kitty;
-    theme = "Catppuccin-Mocha";
+    # theme = "Catppuccin-Mocha";
+    theme = "${lib.strings.concatStringsSep "-"
+      (lib.strings.splitString " " (config.colorScheme.name))}";
     settings = {
       # tab bar janked from https://github.com/catppuccin/kitty
       tab_bar_min_tabs = 1;
@@ -22,6 +28,48 @@
       cursor_stop_blinking_after = "15.0";
 
       scrollback_lines = 5000;
+      # Colors (adapted from: https://github.com/kdrag0n/base16-kitty/blob/master/templates/default-256.mustache)
+      # background = "#${config.colorScheme.palette.base00}";
+      # foreground = "#${config.colorScheme.palette.base05}";
+      # selection_background = "#${config.colorScheme.palette.base05}";
+      # selection_foreground = "#${config.colorScheme.palette.base00}";
+      # url_color = "#${config.colorScheme.palette.base04}";
+      # cursor = "#${config.colorScheme.palette.base05}";
+      # active_border_color = "#${config.colorScheme.palette.base03}";
+      # inactive_border_color = "#${config.colorScheme.palette.base01}";
+      # active_tab_background = "#${config.colorScheme.palette.base00}";
+      # active_tab_foreground = "#${config.colorScheme.palette.base05}";
+      # inactive_tab_background = "#${config.colorScheme.palette.base01}";
+      # inactive_tab_foreground = "#${config.colorScheme.palette.base04}";
+      # tab_bar_background = "#${config.colorScheme.palette.base01}";
+
+      # # normal
+      # color0 = "#${config.colorScheme.palette.base00}";
+      # color1 = "#${config.colorScheme.palette.base08}";
+      # color2 = "#${config.colorScheme.palette.base0B}";
+      # color3 = "#${config.colorScheme.palette.base0A}";
+      # color4 = "#${config.colorScheme.palette.base0D}";
+      # color5 = "#${config.colorScheme.palette.base0E}";
+      # color6 = "#${config.colorScheme.palette.base0C}";
+      # color7 = "#${config.colorScheme.palette.base05}";
+
+      # # bright
+      # color8 = "#${config.colorScheme.palette.base03}";
+      # color9 = "#${config.colorScheme.palette.base08}";
+      # color10 = "#${config.colorScheme.palette.base0B}";
+      # color11 = "#${config.colorScheme.palette.base0A}";
+      # color12 = "#${config.colorScheme.palette.base0D}";
+      # color13 = "#${config.colorScheme.palette.base0E}";
+      # color14 = "#${config.colorScheme.palette.base0C}";
+      # color15 = "#${config.colorScheme.palette.base07}";
+
+      # # extended base16 colors
+      # color16 = "#${config.colorScheme.palette.base09}";
+      # color17 = "#${config.colorScheme.palette.base0F}";
+      # color18 = "#${config.colorScheme.palette.base01}";
+      # color19 = "#${config.colorScheme.palette.base02}";
+      # color20 = "#${config.colorScheme.palette.base04}";
+      # color21 = "#${config.colorScheme.palette.base06}";
     };
     keybindings = {
       "ctrl+c" = "copy_or_interrupt";
