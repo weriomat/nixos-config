f6031b4066b6ba17c38356aad878c7173f131700
weriomat <eliasaengel@gmail.com>
2024-03-13 23:14:09 +0100

    chore: update share

--- modules/nixos/nfs/default.nix
+++ modules/nixos/nfs/default.nix
@@ -1,11 +1,17 @@
 {pkgs, ...}: {
   networking.hosts = {"192.168.178.199" = ["nas"];};
-  # fileSystems."/mnt/zpool5" = {
   fileSystems."/home/marts/nfs" = {
     device = "nas:/mnt/zpool5";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600"];
   };
+
+  fileSystems."/home/marts/share" = {
+    device = "nas:/mnt/zpool5/share";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600"];
+  };
+
   environment.systemPackages = with pkgs; [
     nfs-utils
   ];
