82fa3c43f275793959c943eb7d241394658271d2
weriomat <engel@weriomat.com>
2024-08-11 18:23:40 +0200

    chore: fix man .

--- modules/nixos/common/documentation.nix
+++ modules/nixos/common/documentation.nix
@@ -14,7 +14,11 @@
   config = lib.mkIf config.doc.enable {
     documentation = {
       enable = true;
-      man.enable = true;
+      man = {
+        enable = true;
+        man-db.enable = true;
+        generateCaches = true;
+      };
       dev.enable = lib.mkIf pkgs.stdenv.isLinux true;
       doc.enable = true;
       nixos.enable = lib.mkIf pkgs.stdenv.isLinux true;
