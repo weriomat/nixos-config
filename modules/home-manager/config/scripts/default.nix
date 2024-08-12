6065237ec4246216b0bf87dceb0f239aade38a46
weriomat <engel@weriomat.com>
2024-08-12 12:40:40 +0200

    chore: add fshow, fzfmenu, fb, fif, fho, fhc

--- modules/home-manager/config/scripts/default.nix
+++ modules/home-manager/config/scripts/default.nix
@@ -18,6 +18,117 @@
     '';
   };
 
+  fshow = pkgs.writeShellApplication {
+    name = "fshow";
+    runtimeInputs = with pkgs; [sqlite coreutils fzf gnugrep gawkInteractive gnused];
+    text = ''
+      git log --graph --color=always \
+          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+      fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+          --bind "ctrl-m:execute:
+                    (grep -o '[a-f0-9]\{7\}' | head -1 |
+                    xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                    {}
+          FZF-EOF"
+    '';
+  };
+
+  fzfmenu = pkgs.writeShellApplication {
+    # Stolen from https://github.com/junegunn/fzf/wiki/Examples#fzf-as-dmenu-replacement
+    name = "fzfmenu";
+    runtimeInputs = with pkgs; [fzf toybox kitty bash gawkInteractive];
+    text = ''
+      #!/usr/bin/env bash
+      FZF_DEFAULT_OPTS="--height=100% --layout=reverse --border --no-sort --prompt=\"~ \" --color=dark,hl:red:regular,fg+:white:regular,hl+:red:regular:reverse,query:white:regular,info:gray:regular,prompt:red:bold,pointer:red:bold" exec kitty --class="fzf-menu" -e bash -c "fzf-tmux -m $* < /proc/$$/fd/0 | awk 'BEGIN {ORS=\" \"} {print}' > /proc/$$/fd/1"
+    '';
+  };
+
+  fb = pkgs.writeShellApplication {
+    name = "fb";
+    runtimeInputs = with pkgs; [buku gawkInteractive toybox sqlite];
+    # TODO: fix to work with fzf menu, sqlite3: https://github.com/junegunn/fzf/wiki/Examples#buku
+
+    text = ''
+      #!/usr/bin/env bash
+      # fb - buku bookmarks fzfmenu opener
+      buku -p -f 4 |
+          awk -F $'\t' '{
+              if ($4 == "")
+                  printf "%s \t\x1b[38;5;208m%s\033[0m\n", $2, $3
+              else
+                  printf "%s \t\x1b[38;5;124m%s \t\x1b[38;5;208m%s\033[0m\n", $2, $4, $3
+          }' |
+          fzf --tabstop 1 --ansi -d $'\t' --with-nth=2,3 \
+              --preview-window='bottom:10%' --preview 'printf "\x1b[38;5;117m%s\033[0m\n" {1}' |
+              awk '{print $1}' | xargs -d '\n' -I{} -n1 -r xdg-open '{}'
+
+    '';
+  };
+
+  fif = pkgs.writeShellApplication {
+    name = "fif";
+    runtimeInputs = with pkgs; [ripgrep-all toybox coreutils];
+
+    text = ''
+      #!/usr/bin/env bash
+      if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+      file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$*" | fzf-tmux +m --preview="rga --ignore-case --pretty --context 10 '""$*""' {}")" && echo "opening $file" && hx "$file" || return 1;
+
+    '';
+  };
+
+  # fbu = pkgs.writeShellApplication {
+  #   name = "fbu";
+  #   runtimeInputs = with pkgs; [buku gawkInteractive toybox sqlite fzf];
+  #   text = ''
+  #     #!/usr/bin/env bash
+
+  #     # BUKU bookmark manager
+  #     # get bookmark ids
+  #     get_buku_ids() {
+  #         buku -p -f 5 | fzf --tac --layout=reverse-list -m | \
+  #           cut -d $'\t' -f 1
+  #     }
+
+  #     # buku update
+  #     # save newline separated string into an array
+  #     mapfile -t ids < <(get_buku_ids)
+
+  #     echo buku --update ${"ids[@]"} "$@"
+
+  #     # [[ -z "${ids [0]}" ]] && return 0 # return if has no bookmark selected
+
+  #     buku --update ${"ids[@]"} "$@"
+
+  #   '';
+  # };
+
+  # fbw = pkgs.writeShellApplication {
+  #   name = "fbw";
+  #   runtimeInputs = with pkgs; [buku gawkInteractive toybox sqlite fzf];
+  #   text = ''
+  #     #!/usr/bin/env bash
+
+  #     # BUKU bookmark manager
+  #     # get bookmark ids
+  #     get_buku_ids() {
+  #         buku -p -f 5 | fzf --tac --layout=reverse-list -m | \
+  #           cut -d $'\t' -f 1
+  #     }
+
+  #     # buku update
+  #     # save newline separated string into an array
+  #     ids=( $(get_buku_ids) )
+
+  #     # update websites
+  #     for i in ${ids[@]}; do
+  #         echo buku --write "$i"
+  #         buku --write "$i"
+  #     done
+
+  #   '';
+  # };
+
   fh = pkgs.writeShellApplication {
     name = "fh";
     runtimeInputs = with pkgs; [sqlite coreutils fzf gnugrep gawkInteractive gnused];
@@ -41,8 +152,7 @@
       awk -F $sep '{printf "%s  \x1b[36m%s  \x1b[m%s\n", $1, $2, $3}' |
       sed -E 's/\x1b\[[0-9;]+m  //g' |
       fzf --ansi --multi |
-      grep -oP 'https?://.*$' |
-      xargs firefox
+      grep -oP 'https?://.*$'
     '';
   };
 
