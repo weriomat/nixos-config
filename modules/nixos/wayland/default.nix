ea72e3fd3336bd2c721d8a530706bd3cc7fe3840
Elias <e.engel@campus.tu-berlin.de>
2024-08-26 22:45:14 +0200

    chore: remove duplicate definition of keys

--- modules/nixos/wayland/default.nix
+++ modules/nixos/wayland/default.nix
@@ -1,13 +1,3 @@
 {...}: {
   imports = [./wayland.nix ./greetd.nix];
-  services = {
-    printing = {
-      # Enable CUPS to print documents.
-      enable = true;
-    };
-    dbus.enable = true;
-    gvfs.enable = true;
-    # gnome.gnome-keyring.enable = true;
-  };
-  # programs.ssh.startAgent = true;
 }
