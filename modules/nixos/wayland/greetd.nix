f952c16a5c59429be28bdaca2279514e332016ae
Elias <e.engel@campus.tu-berlin.de>
2024-08-26 22:46:34 +0200

    chore: cleanup greetd

--- modules/nixos/wayland/greetd.nix
+++ modules/nixos/wayland/greetd.nix
@@ -3,16 +3,8 @@
   globals,
   ...
 }: {
-  # display manager
   services.greetd = {
     enable = true;
-    # settings = {
-    # default_session = {
-    #   command =
-    #     "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland -s Hyprland";
-    #   user = "greeter";
-    # };
-    # };
     settings = rec {
       initial_session = {
         command = "${pkgs.hyprland}/bin/Hyprland";
