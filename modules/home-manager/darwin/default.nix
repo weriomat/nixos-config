bc1b44152a85c980ec6c9391db978b699d194e26
Elias <e.engel@campus.tu-berlin.de>
2024-08-26 20:46:00 +0200

    chore: cleanup darwin-hm

--- modules/home-manager/darwin/default.nix
+++ modules/home-manager/darwin/default.nix
@@ -1,31 +1,14 @@
 {inputs, ...}: {
   colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
+  # we dont have to disable anything since it is disables by default
+
+  catppuccin = {
+    enable = true;
+    accent = "mauve";
+    flavor = "mocha";
+  };
   home = {
     stateVersion = "23.11";
-    sessionVariables = {
-      # EDITOR = "emacs";
-    };
-    file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
-    };
-    packages = [
-      # overlays -> e.g just install one nerd font
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-      # write shell script like this
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
-    ];
   };
   programs.home-manager.enable = true;
 }
