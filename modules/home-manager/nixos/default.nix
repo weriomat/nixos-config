e1e36b0c8622ed89422ec5ec22b8b0fbb368d60e
Elias <e.engel@campus.tu-berlin.de>
2024-08-26 20:45:54 +0200

    chore: cleanup nixos-hm

--- modules/home-manager/nixos/default.nix
+++ modules/home-manager/nixos/default.nix
@@ -21,32 +21,11 @@
   colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
 
   home = rec {
-    # username = inherit globals.username;
     inherit (globals) username;
     homeDirectory = "/home/${username}";
     stateVersion = "23.11"; # Has not to be changed
     packages = builtins.attrValues (import ../config/scripts {inherit pkgs globals;});
   };
-
-  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-  # (pkgs.writeShellScriptBin "my-hello" ''
-  #   echo "Hello, ${config.home.username}!"
-  # '')
-  # ];
-
-  # home.file = {
-  # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-  # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-  # # symlink to the Nix store copy.
-  # ".screenrc".source = dotfiles/screenrc;
-
-  # # You can also set the file content immediately.
-  # ".gradle/gradle.properties".text = ''
-  #   org.gradle.console=verbose
-  #   org.gradle.daemon.idletimeout=3600000
-  # '';
-  # };
 
   # home.sessionVariables = {
   # EDITOR = "emacs";
