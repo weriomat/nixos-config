dbfe9c419fa82b3efe83930b52357fbd96040fb7
weriomat <engel@weriomat.com>
2024-09-01 18:59:35 +0200

    chore: add ./local/bin to $PATH

--- modules/home-manager/nixos/default.nix
+++ modules/home-manager/nixos/default.nix
@@ -25,6 +25,11 @@
     homeDirectory = "/home/${username}";
     stateVersion = "23.11"; # Has not to be changed
     packages = builtins.attrValues (import ../config/scripts {inherit pkgs globals inputs;});
+    # Add ./local/bin to $PATH
+    sessionPath = [
+      "$HOME/.cargo/bin"
+      "$HOME/.local/bin"
+    ];
   };
 
   # home.sessionVariables = {
