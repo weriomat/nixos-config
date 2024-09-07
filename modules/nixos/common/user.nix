e6c2e934c1f2d6c3352f89726f365eaf90073657
weriomat <engel@weriomat.com>
2024-09-08 01:36:51 +0200

    chore: rework user.nix

--- modules/nixos/common/user.nix
+++ modules/nixos/common/user.nix
@@ -3,40 +3,37 @@
   globals,
   ...
 }: {
-  config = {
-    time.timeZone = "Europe/Berlin";
+  time.timeZone = "Europe/Berlin";
 
+  users = {
     users = {
-      users = {
-        ${globals.username} = {
-          isNormalUser = true;
-          createHome = true;
-          description = "${globals.username}";
+      ${globals.username} = {
+        isNormalUser = true;
+        createHome = true;
+        description = "${globals.username}";
 
-          # TODO: add etra groups
-          extraGroups =
-            [
-              "networkmanager"
-              "wheel"
-              "network"
-              "dialout"
-              "disk"
-              "audio"
-              "video"
-              "disk"
-              "input"
-            ]
-            ++ globals.extraGroups;
+        extraGroups =
+          [
+            "networkmanager"
+            "wheel"
+            "network"
+            "dialout"
+            "disk"
+            "audio"
+            "video"
+            "disk"
+            "input"
+          ]
+          ++ globals.extraGroups;
 
-          uid = 1000;
-          homeMode = "755";
-        };
+        uid = 1000;
+        homeMode = "755";
       };
-      defaultUserShell = pkgs.zsh;
-      # TODO: make pasword file here
-      # mutableUsers = false;
     };
-
-    programs.zsh.enable = true;
+    defaultUserShell = pkgs.zsh;
+    # TODO: make pasword file here
+    # mutableUsers = false;
   };
+
+  programs.zsh.enable = true;
 }
