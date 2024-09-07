43ff6a987eb82d686ddd586c45927624f5151aac
weriomat <engel@weriomat.com>
2024-09-08 01:35:18 +0200

    chore: refactor audio module

--- modules/nixos/common/audio.nix
+++ modules/nixos/common/audio.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -9,26 +8,30 @@ with lib; {
 
   config = mkIf config.audio.enable {
     sound.enable = true;
-    hardware.pulseaudio = {
-      enable = false;
-      package = pkgs.pulseaudioFull;
-    };
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      wireplumber.enable = true;
+
+    services = {
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        wireplumber.enable = true;
+      };
+
+      blueman.enable = true;
     };
 
-    services.blueman.enable = true;
-    hardware.bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-      settings.General = {
-        Enable = "Source,Sink,Media,Socket";
-        Experimental = true;
+    hardware = {
+      pulseaudio.enable = false;
+
+      bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings.General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
       };
     };
   };
