7212b89d10538191acfe1cf340e2b2f6942735ca
weriomat <engel@weriomat.com>
2024-09-08 01:37:59 +0200

    chore: bump

--- modules/nixos/common/languagetool.nix
+++ modules/nixos/common/languagetool.nix
@@ -1,7 +1,7 @@
 {globals, ...}: {
   # TODO: make this a module with cacheSize
   services.languagetool = {
-    # dont forget to swtich to local server in firefox
+    # NOTE: do not forget to set this server as source in firefox
     enable = true;
     port = 8081;
     public = false;
