08c800b15a3fa45fc2944aed186dbdb7d9a63fba
weriomat <engel@weriomat.com>
2024-09-08 16:54:40 +0200

    chore: disable mutable users and enable hashedPasswd

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -11,7 +11,6 @@
         isNormalUser = true;
         createHome = true;
         description = "${globals.username}";
-        homeMode = "755";
         extraGroups = [
           "networkmanager"
           "tty"
@@ -24,14 +23,14 @@
           "disk"
           "input"
         ];
-
+        hashedPassword = "$y$j9T$NbSJKRlQhIdwZauz4m2uX1$AdlYNcSkXN2DWZ0Q/kETh/lUrUGS5.3IMs.ReoLD2NB";
         uid = 1000;
         homeMode = "755";
       };
     };
     defaultUserShell = pkgs.zsh;
     # TODO: make pasword file here
-    # mutableUsers = false;
+    mutableUsers = false;
   };
 
   programs.zsh.enable = true;
