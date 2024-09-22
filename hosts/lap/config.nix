f2d563f0aa45b66a0fbd1eeb49d4a5960c9519e9
weriomat <engel@weriomat.com>
2024-09-22 16:33:13 +0200

    chore: add smartd ntfy for laptop host

--- hosts/lap/config.nix
+++ hosts/lap/config.nix
@@ -1,16 +1,33 @@
 {
-  lib,
   globals,
+  lib,
   ...
 }: {
   # NOTE: disbale virtualiziation
   virt.enable = lib.mkForce false;
 
-  # TODO: zfs mail
+  # TODO: for every host a vpn as a split tunnel for email relay or sth: https://ssh.sshslowdns.com/wireguard-split-tunnel-config/
+  # TODO: setup email with host
+
+  # TODO: smartd for other hosts
+  # TODO: upstream
+  # TODO: https://rair.dev/zfs-smart-ntfy/
+  sops.secrets."ntfy" = {};
+
   services = {
+    # TODO: setup mail as a relay though vps
+    msmartd.enable = true;
+
     zfs = {
       trim.enable = true;
       autoScrub.enable = true;
+      # TODO: here -> probably needs a patch
+      # TODO: upsteam a patch?
+      # zed.settings = {
+      #   ZED_NTFY_TOPIC = "zfs";
+      #   ZED_NTFY_URL = "https://ntfy.weriomat.com";
+      #   ZED_NTFY_ACCESS_TOKEN = "$(cat ${config.sops.secrets."ntfy".path})";
+      # };
     };
 
     # set cache size for laglanguagetool to 8gb
@@ -21,9 +38,9 @@
 
     borgbackup.jobs."hetzner" = {
       doInit = lib.mkForce false;
-      # exclude = [];
     };
   };
+
   networking.hostId = "fb363d05";
 
   # NOTE: kanshi is configured at the host level
