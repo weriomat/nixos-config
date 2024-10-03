361eac1a7527dd562d7528e2d6902d6c4e68cea2
Elias <e.engel@campus.tu-berlin.de>
2024-10-03 12:39:09 +0200

    chore: fix apple emoji font

--- pkgs/apple-color-emoji/default.nix
+++ pkgs/apple-color-emoji/default.nix
@@ -3,50 +3,29 @@ pkgs.stdenv.mkDerivation {
   pname = "apple-emoji";
   version = "0.0.1";
 
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "samuelngs";
-  #   repo = "apple-emoji-linux";
-  #   rev = "e40e6d35657b908f473faed8f6461e8c54d01420";
-  #   hash = "sha256-liklPjOJhHOBWQH8AQwkLfIG0KIqdnZcVAa7oMrVZMk=";
-  # };
-  src = pkgs.fetchurl {
-    url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v17.4/AppleColorEmoji.ttf";
-    hash = "sha256-SG3JQLybhY/fMX+XqmB/BKhQSBB0N1VRqa+H6laVUPE=";
+  src = pkgs.fetchFromGitHub {
+    owner = "samuelngs";
+    repo = "apple-emoji-linux";
+    rev = "e40e6d35657b908f473faed8f6461e8c54d01420";
+    hash = "sha256-liklPjOJhHOBWQH8AQwkLfIG0KIqdnZcVAa7oMrVZMk=";
   };
 
-  # buildInputs = with pkgs; [optipng pngquant python312Packages.fonttools];
-  # runtimeInputs = with pkgs; [optipng];
+  buildInputs = with pkgs; [which imagemagick pngquant zopfli python312Packages.fonttools python312Packages.nototools];
 
-  # skipPhases = ["unpackPhase"];
-  dontUnpack = true;
-  # unpackPhase = ''
-  # '';
-  # TODO: here
+  buildPhase = ''
+    make -j
+  '';
+
   installPhase = ''
     runHook preInstall
 
-    install -Dm644 *.ttf -t $out/share/fonts/truefont
+    mkdir -p $out/share/fonts/truefont
+    install -Dm644 AppleColorEmoji.ttf -t $out/share/fonts/truefont
 
     runHook postInstall
   '';
 
-  description = "Apple Emoji for Linux";
+  description = "Brings Apple's vibrant emojis to your Linux experience";
+  homepage = "https://github.com/samuelngs/apple-emoji-linux";
+  mainProgram = "apple-emoji-linux";
 }
-# stdenv.mkDerivation rec {
-#   pname = "apple-emoji-linux";
-#   version = "17.4";
-#   src = fetchFromGitHub {
-#     owner = "samuelngs";
-#     repo = "apple-emoji-linux";
-#     rev = "v${version}";
-#     hash = "sha256-liklPjOJhHOBWQH8AQwkLfIG0KIqdnZcVAa7oMrVZMk=";
-#   };
-#   meta = with lib; {
-#     description = "Brings Apple's vibrant emojis to your Linux experience";
-#     homepage = "https://github.com/samuelngs/apple-emoji-linux";
-#     license = licenses.asl20;
-#     maintainers = with maintainers; [ ];
-#     mainProgram = "apple-emoji-linux";
-#     platforms = platforms.all;
-#     };
-
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@
   # TODO: switch to pkgs.callPackage
   weriomat-wallpapers = import ./weriomat-wallpapers {inherit pkgs;};
   # TODO: here
-  # apple-emoji = import ./apple-color-emoji {inherit pkgs;};
+  apple-emoji = import ./apple-color-emoji {inherit pkgs;};
   # TODO: signal desktop
   # https://github.com/gvolpe/nix-config/blob/b9ff455faaf5a4890985305e5c7a5a01606d20f3/home/modules/signal.nix
 }
