f536bb7d4564de1b78891a1779d5b5c889e4c841
weriomat <engel@weriomat.com>
2024-10-16 20:38:08 +0200

    chore: cleanup power.nix

--- hosts/lap/power.nix
+++ hosts/lap/power.nix
@@ -18,45 +18,31 @@
     upower.enable = true;
     tlp = {
       enable = true;
-      # TODO: here
-
-      # settings = {
-      #   DISK_SPINDOWN_TIMEOUT_ON_AC = "keep 1";
-      #   DISK_SPINDOWN_TIMEOUT_ON_BAT = "keep 1";
-
-      #   DISK_APM_LEVEL_ON_BAT = "keep 127";
-      #   DISK_APM_LEVEL_ON_AC = "keep 127";
-
-      #   SATA_LINKPWR_ON_AC = "med_power_with_dipm min_power";
-      #   SATA_LINKPWR_ON_BAT = "med_power_with_dipm min_power";
-
-      #   AHCI_RUNTIME_PM_ON_AC = "on";
-      #   AHCI_RUNTIME_PM_ON_BAT = "on";
-
-      #   AHCI_RUNTIME_PM_TIMEOUT = "6";
-
-      #   RUNTIME_PM_ON_AC = "auto";
-      #   RUNTIME_PM_ON_BAT = "auto";
-      # };
       settings = {
+        # TODO: take a look at other settings
+        # NOTE: tipps from here https://linrunner.de/tlp/support/optimizing.html
+
         # CPU handling
+        CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
         CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-        SCHED_POWERSAVE_ON_AC = 0;
-        SCHED_POWERSAVE_ON_BAT = 1;
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
 
         # Battery care
         START_CHARGE_THRESH_BAT0 = 70;
         STOP_CHARGE_THRESH_BAT0 = 80;
+        RUNTIME_PM_ON_AC = "auto";
         RUNTIME_PM_ON_BAT = "auto";
 
         # Devices
         DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wwan";
         DEVICES_TO_ENABLE_ON_STARTUP = "wifi";
-        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi";
+        # DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi";
+        # DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi";
       };
     };
   };
