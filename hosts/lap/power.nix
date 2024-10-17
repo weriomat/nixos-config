68178ddf2badca2b57a8c891213cd92652d6710c
weriomat <engel@weriomat.com>
2024-10-17 22:16:01 +0200

    chore: switch scaling governor on bat to powersave
    apperently schedutils does not exist

--- hosts/lap/power.nix
+++ hosts/lap/power.nix
@@ -25,7 +25,7 @@
         # CPU handling
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
