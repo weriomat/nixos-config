5440bcc0100e2abfc5af63837fb455e806fc9aed
weriomat <engel@weriomat.com>
2024-10-17 19:52:36 +0200

    chore: rotate gpg key

--- hosts/lap/ssh.nix
+++ hosts/lap/ssh.nix
@@ -40,7 +40,8 @@
         };
         signing = {
           signByDefault = true;
-          key = "008F5FA7F0C2803D";
+          # key = "008F5FA7F0C2803D";
+          key = "E64AE4E613602685";
         };
       };
       ssh = {
