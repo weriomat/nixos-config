70dc97c69305eaa48b794d6739a2e93367f5204b
weriomat <engel@weriomat.com>
2024-11-08 13:38:09 +0100

    chore: fix hyprland setup

--- system/nixos/wayland/default.nix
+++ system/nixos/wayland/default.nix
@@ -1,43 +1,30 @@
 {
+  lib,
   pkgs,
   globals,
   ...
-}: {
-  # TODO: here idk what most of this means
-  programs = {
-    hyprland = {
-      enable = true;
-      #   package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    };
-    xwayland.enable = true;
-  };
+}: let
+  inherit (lib) mkForce;
+  # NOTE: this is the version used by home-manager
+  # hyprland = config.home-manager.users.${globals.username}.wayland.windowManager.hyprland.finalPackage;
+  inherit (pkgs) hyprland;
+in {
+  imports = [./gnome.nix];
 
-  # TODO: here
-  # greetd = {
-  #    enable = true;
-  #    settings = rec {
-  #      regreet_session = {
-  #        command = "${lib.exe pkgs.cage} -s -- ${lib.exe pkgs.greetd.regreet}";
-  #        user = "greeter";
-  #      };
-  #      tuigreet_session =
-  #        let
-  #          session = "${pkgs.hyprland}/bin/Hyprland";
-  #          tuigreet = "${lib.exe pkgs.greetd.tuigreet}";
-  #        in
-  #        {
-  #          command = "${tuigreet} --time --remember --cmd ${session}";
-  #          user = "greeter";
-  #        };
-  #      default_session = tuigreet_session;
-  #    };
-  #  };
+  programs.xwayland.enable = true; # Enable simulation of x11
+
+  hardware.opengl.enable = lib.mkDefault true;
 
   services = {
+    # Window manager only sessions (unlike DEs) don't handle XDG
+    # autostart files, so force them to run the service
+    xserver.desktopManager.runXdgAutostartIfNone = lib.mkDefault true;
+
     printing = {
       enable = true;
       drivers = with pkgs; [brlaser brgenml1lpr brgenml1cupswrapper gutenprint gutenprintBin];
     };
+
     dbus.enable = true;
     gvfs.enable = true;
 
@@ -45,8 +32,21 @@
     greetd = {
       enable = true;
       settings = rec {
+        #      regreet_session = {
+        #        command = "${lib.exe pkgs.cage} -s -- ${lib.exe pkgs.greetd.regreet}";
+        #        user = "greeter";
+        #      };
+        #      tuigreet_session =
+        #        let
+        #          session = "${pkgs.hyprland}/bin/Hyprland";
+        #          tuigreet = "${lib.exe pkgs.greetd.tuigreet}";
+        #        in
+        #        {
+        #          command = "${tuigreet} --time --remember --cmd ${session}";
+        #          user = "greeter";
+        #        };
         initial_session = {
-          command = "${pkgs.hyprland}/bin/Hyprland";
+          command = "${hyprland}/bin/Hyprland";
           user = "${globals.username}";
         };
         default_session = initial_session;
@@ -54,18 +54,16 @@
     };
   };
 
-  # NOTE: maybe move this into hm?
   # TODO: here https://wiki.hyprland.org/Useful-Utilities/Hyprland-desktop-portal/
-  # portal for sharing (file pickers)
+  # portal for sharing (file pickers), gtk will be enabled by gnome
   xdg.portal = {
     enable = true;
-    # TODO: here
-    wlr.enable = true;
+    wlr.enable = mkForce false; # hyprland has its own portal
     xdgOpenUsePortal = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
+      (pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;})
     ];
+    configPackages = [hyprland];
   };
 
   # Enable polkit
@@ -75,11 +73,18 @@
   };
 
   # Enable polkit for system wide auth, required as part of gnome-compat
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-    description = "Gnome polkit agent";
-    partOf = ["graphical-session.target"];
-    script = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-    unitConfig = {ConditionUser = "!@system";};
+  systemd.user = {
+    services.polkit-gnome-authentication-agent-1 = {
+      description = "Gnome polkit agent";
+      partOf = ["graphical-session.target"];
+      script = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      unitConfig = {ConditionUser = "!@system";};
+    };
+
+    # Set default hyprland environment
+    extraConfig = ''
+      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:$PATH"
+    '';
   };
   environment.systemPackages = with pkgs; [polkit_gnome];
 }
