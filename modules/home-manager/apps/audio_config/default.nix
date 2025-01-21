b7c00a6a76361a99635f16f48226f03d402cd6b3
weriomat <engel@weriomat.com>
2025-01-21 13:32:24 +0100

    chore: refactor audio_config, add script to toggle mute, setup brightnesskeys

--- modules/home-manager/apps/audio_config/default.nix
+++ modules/home-manager/apps/audio_config/default.nix
@@ -1,69 +1,108 @@
 {
   config,
+  globals,
   lib,
   pkgs,
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkMerge
+    getExe
+    ;
+  toggle-mic = pkgs.writeShellApplication {
+    name = "toggle-mic";
+    runtimeInputs = with pkgs; [
+      pamixer
+      libnotify
+      gnugrep
+      gawk
+      coreutils
+    ];
+
+    text = ''
+      #!/usr/bin/env bash
+      if ! command -v pamixer > /dev/null; then
+          exit_code="$?"
+
+          if [ "$notify" ]; then
+              notify-send \
+                  --hint=int:transient:1 \
+                  "Error" \
+                  "'pamixer' is not available."
+          fi
+
+          exit $exit_code
+      fi
+
+      pamixer --list-sources \
+          | grep input \
+          | awk '{print $1}' \
+          | while read -r index; do
+          mic_muted="$(pamixer --source "$index" --get-mute)"
+
+          if [ "$mic_muted" = "true" ]; then
+              pamixer --source "$index" -u
+          else
+              pamixer --source "$index" -m
+          fi
+
+          word=$(pamixer --list-sources | grep input | grep "$index" | awk '{print $4}')
+          line=$(pamixer --list-sources | grep input | grep "$index" | grep -o "''${word}[^\"]*" | grep -o "[^\"]*")
+          notify-send --hint=int:transient:1 "$line" "Toggled"
+      done
+    '';
+  };
 in
 {
   options.audio_config.enable = mkEnableOption "Enable HomeManager audio setup";
 
+  # NOTE: this configures brightness stuff as well
   config = mkIf config.audio_config.enable {
-    # TODO: factor out from waybar audio-idle-ihnhibit
     home.packages = with pkgs; [
-      paprefs # pulseaudio preferences
       pasystray # pulseaudio systray
       pavucontrol # pulseaudio volume control
       playerctl # music player controller
       pulsemixer # pulseaudio mixer
       pamixer
     ];
+
     # TODO: audio
     # services.pulseeffects = {};
     # services.playerctld = {};
     # services.pasystray = {};
     # services.easyeffects = {};
 
-    # TODO: swtich to lib.getEXE
     wayland.windowManager.hyprland.settings = {
-      # NOTE: maybe configure `XF86MonBrightness<Up|Down>` as well if need be for controlling backlight of a keyboard
       exec-once = [
-        "${pkgs.sway-audio-idle-inhibit}/bin/sway-audio-idle-inhibit &"
+        "${getExe pkgs.sway-audio-idle-inhibit} &"
       ];
+
+      # NOTE: the [e] means repeating
+      binde = mkMerge [
+        [
+          ",XF86AudioRaiseVolume, exec, ${getExe pkgs.pamixer} -i 2"
+          ",XF86AudioLowerVolume, exec, ${getExe pkgs.pamixer} -d 2"
+          ",XF86AudioNext, exec, ${getExe pkgs.playerctl} next"
+          ",XF86AudioPrev, exec, ${getExe pkgs.playerctl} previous"
+        ]
+        (mkIf globals.laptop [
+          ",XF86MonBrightnessUp, exec, ${getExe pkgs.brightnessctl} set +5%"
+          ",XF86MonBrightnessDown, exec, ${getExe pkgs.brightnessctl} set -5% "
+
+        ])
+      ];
+
       bind = [
-        ",XF86AudioRaiseVolume, exec, ${pkgs.pamixer}/bin/pamixer -i 2"
-        ",XF86AudioLowerVolume, exec, ${pkgs.pamixer}/bin/pamixer -d 2"
-        ",XF86AudioMute, exec, ${pkgs.pamixer}/bin/pamixer -t"
-        ",XF86AudioPlay, exec, ${pkgs.playerctl}/bin/playerctl play-pause"
-        ",XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next"
-        ",XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous"
-        ",XF86AudioStop, exec, ${pkgs.playerctl}/bin/playerctl stop"
+        ",XF86AudioMute, exec, ${getExe pkgs.pamixer} -t"
+        ",XF86AudioMicMute, exec, ${getExe toggle-mic}"
+        ",XF86AudioPlay, exec, ${getExe pkgs.playerctl} play-pause"
+        ",XF86AudioStop, exec, ${getExe pkgs.playerctl} stop"
       ];
 
       # TODO: maybe this in per host config as well?
-      # bindle = [
-      #   # volume
-      #   ", XF86AudioRaiseVolume, exec, wpctl set-volume -l '1.0' @DEFAULT_AUDIO_SINK@ 6%+"
-      #   ", XF86AudioLowerVolume, exec, wpctl set-volume -l '1.0' @DEFAULT_AUDIO_SINK@ 6%-"
-
-      #   # backlight
-      #   ", XF86MonBrightnessUp, exec, brillo -q -u 300000 -A 5"
-      #   ", XF86MonBrightnessDown, exec, brillo -q -u 300000 -U 5"
-      # ];
-      # bindi = {
-      #   ",XF86MonBrightnessUp" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl +5%";
-      #   ",XF86MonBrightnessDown" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl -5% ";
-      #   ",XF86AudioRaiseVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -i 5";
-      #   ",XF86AudioLowerVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -d 5";
-      #   ",XF86AudioMute" = "exec, ${pkgs.pamixer}/bin/pamixer --toggle-mute";
-      #   ",XF86AudioMicMute" = "exec, ${pkgs.pamixer}/bin/pamixer --default-source --toggle-mute";
-      #   ",XF86AudioNext" = "exec,playerctl next";
-      #   ",XF86AudioPrev" = "exec,playerctl previous";
-      #   ",XF86AudioPlay" = "exec,playerctl play-pause";
-      #   ",XF86AudioStop" = "exec,playerctl stop";
-      # };
       #   windowrule = float,audacious
       #   windowrule = workspace 8 silent, audacious
       #   windowrule = float,title:^(Volume Control)$
