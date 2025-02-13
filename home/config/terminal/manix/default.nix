5949a6d2ea999dded2c43d38f272e5c76c854417
weriomat <engel@weriomat.com>
2025-02-13 12:32:38 +0100

    chore: remove with from manix

--- home/config/terminal/manix/default.nix
+++ home/config/terminal/manix/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ manix ];
+  home.packages = [ pkgs.manix ];
   # TODO: here paths
   programs.zsh.shellAliases = {
     ma = ''${pkgs.manix}/bin/manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix'';
