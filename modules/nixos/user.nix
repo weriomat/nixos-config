46370992896f623e22c2c71416d24492d27180fa
weriomat <engel@weriomat.com>
2025-02-23 21:20:46 +0100

    chore: add group for user and set group to user

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -7,28 +7,29 @@
   time.timeZone = "Europe/Berlin";
 
   users = {
-    users = {
-      ${globals.username} = {
-        isNormalUser = true;
-        createHome = true;
-        description = "${globals.username}";
-        extraGroups = [
-          "networkmanager"
-          "tty"
-          "wheel"
-          "network"
-          "dialout"
-          "disk"
-          "audio"
-          "video"
-          "disk"
-          "input"
-        ];
-        hashedPassword = "$y$j9T$NbSJKRlQhIdwZauz4m2uX1$AdlYNcSkXN2DWZ0Q/kETh/lUrUGS5.3IMs.ReoLD2NB";
-        uid = 1000;
-        homeMode = "755";
-      };
+    users.${globals.username} = {
+      isNormalUser = true;
+      createHome = true;
+      description = "${globals.username}";
+      extraGroups = [
+        "networkmanager"
+        "tty"
+        "wheel"
+        "network"
+        "dialout"
+        "disk"
+        "audio"
+        "video"
+        "disk"
+        "input"
+      ];
+      hashedPassword = "$y$j9T$NbSJKRlQhIdwZauz4m2uX1$AdlYNcSkXN2DWZ0Q/kETh/lUrUGS5.3IMs.ReoLD2NB";
+      uid = 1000;
+      homeMode = "755";
+      group = globals.username;
     };
+    groups.${globals.username} = { };
+
     defaultUserShell = pkgs.zsh;
     mutableUsers = false;
   };
