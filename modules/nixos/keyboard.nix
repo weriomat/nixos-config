026150310cd637322ac5d381846b81108546f331
weriomat <engel@weriomat.com>
2025-03-28 22:34:36 +0100

    chore: enable solaar a manager for logitech mice

--- modules/nixos/keyboard.nix
+++ modules/nixos/keyboard.nix
@@ -1,20 +1,29 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 let
   inherit (lib) mkEnableOption mkIf;
 in
 {
-  options.keyboard.enable = mkEnableOption "Enable keyboard properties";
+  options.keyboard.enable = mkEnableOption "Keyboard/ Mouse properties";
 
   config = mkIf config.keyboard.enable {
-    # Blacklist logitech usb receiver from USB autosuspend
-    services.udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c548", GOTO="power_usb_rules_end"
-      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto", LABEL="power_usb_rules_end"
-    '';
+    hardware.logitech.wireless = {
+      # Extra Logitech Support
+      enable = true;
+      enableGraphical = true;
+    };
+    services.udev = {
+      packages = [ pkgs.solaar ];
+      # Blacklist logitech usb receiver from USB autosuspend
+      extraRules = ''
+        ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c548", GOTO="power_usb_rules_end"
+        ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto", LABEL="power_usb_rules_end"
+      '';
+    };
 
     i18n = {
       defaultLocale = "en_US.UTF-8";
