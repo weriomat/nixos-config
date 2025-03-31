6d8da7f74767f31749cbd07f4ac4db3ef8d9868b
weriomat <engel@weriomat.com>
2025-03-31 22:20:41 +0200

    chore: cleanup discord

--- modules/home-manager/apps/discord/default.nix
+++ modules/home-manager/apps/discord/default.nix
@@ -8,34 +8,12 @@ let
   inherit (lib) mkEnableOption mkIf;
 in
 {
-  # TODO: fix this
-  # imports = [ (import ./theme-template.nix) ];
   imports = [
     inputs.nixcord.homeManagerModules.nixcord
   ];
   options.discord.enable = mkEnableOption "Enable discord overlay";
 
   config = mkIf config.discord.enable {
-    #   home.packages = [
-    #     # vesktop
-    #     # https://github.com/Goxore/nixconf/blob/main/homeManagerModules/features/vesktop.nix
-    #     # TODO: catppucin, catppuccin-discord https://github.com/NixOS/nixpkgs/pull/365773/commits/db285865f73b3214f13536c32aa729bb4de77baa
-    #     (pkgs.discord.override {
-    #       withOpenASAR = true;
-    #       withVencord = true;
-    #     })
-    #     # discord
-    #   ];
-    #
-    # css for discord
-    # programs.discocss = {
-    #   enable = true;
-    #   css = ''
-    #     @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css");
-    #   '';
-    #   discordAlias = true; # alias discocss to discord
-    #   discordPackage = pkgs.discord;
-    # };
     # NOTE: install: enable all the plugins, add https://catppuccin.github.io/discord/dist/catppuccin-mocha-pink.theme.css as theme
     programs.nixcord = {
       enable = true;
