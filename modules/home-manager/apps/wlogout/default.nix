3dd498a7fd4093f8375ac445245f1b5e3718dfe1
weriomat <engel@weriomat.com>
2025-04-07 17:16:06 +0200

    chore: bump notes

--- modules/home-manager/apps/wlogout/default.nix
+++ modules/home-manager/apps/wlogout/default.nix
@@ -21,7 +21,6 @@ in
       layout = [
         {
           label = "lock";
-          # TODO: suspend
           action = "${pkgs.coreutils}/bin/sleep 0.25; ${config.programs.hyprlock.package}/bin/hyprlock";
           text = "Lock";
           keybind = "l";
