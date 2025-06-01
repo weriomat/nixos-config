995f6198587a8bd77ffbbe8cb49ae058083a5cec
weriomat <engel@weriomat.com>
2025-06-01 13:09:24 +0200

    chore: add ipv6 dns server

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -148,9 +148,17 @@ in
       domain = "weriomat.com";
       # NOTE: DoT -> `https://shivering-isles.com/2021/01/configure-dot-on-systemd-resolved`
       nameservers = mkIf cfg.dns.enable [
+        # see https://developers.cloudflare.com/1.1.1.1/ip-addresses/#1111
         "1.1.1.1#one.one.one.one"
         "1.0.0.1#one.one.one.one"
+        "2606:4700:4700::1111#one.one.one.one"
+        "2606:4700:4700::1001#one.one.one.one"
+
+        # see https://www.quad9.net/service/service-addresses-and-features
         "9.9.9.9#dns.quad9.net"
+        "149.112.112.112#dns.quad9.net"
+        "2620:fe::fe#dns.quad9.net"
+        "2620:fe::9#dns.quad9.net"
       ];
       dhcpcd.extraConfig = mkIf cfg.dns.enable "nohook resolv.conf";
       resolvconf.enable = mkIf cfg.dns.enable (mkForce false);
