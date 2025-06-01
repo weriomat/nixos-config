b82fdb541497fa781ef2716432e6a0364206c8c4
weriomat <engel@weriomat.com>
2025-06-01 13:09:12 +0200

    chore: move macchanger arround

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -113,6 +113,21 @@ in
       };
     };
 
+    # TODO: Take a look at https://github.com/XNM1/linux-nixos-hyprland-config-dotfiles/blob/main/nixos/mac-randomize.nix
+    # # Enable MAC Randomize
+    # systemd.services.macchanger = {
+    #   enable = true;
+    #   description = "Change MAC address";
+    #   wantedBy = [ "multi-user.target" ];
+    #   after = [ "network.target" ];
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #     ExecStart = "${pkgs.macchanger}/bin/macchanger -r wlp0s20f3";
+    #     ExecStop = "${pkgs.macchanger}/bin/macchanger -p wlp0s20f3";
+    #     RemainAfterExit = true;
+    #   };
+    # };
+
     # from https://insanity.industries/post/simple-networking/
     services.udev.extraRules = builtins.concatStringsSep "\n" [
       (optionalString cfg.iwd.enable ''
