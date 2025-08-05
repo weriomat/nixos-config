336e789b029eb43373810b0b373edd4a9bc2619d
weriomat <engel@weriomat.com>
2025-08-05 17:02:26 +0200

    chore: add activemq

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -22,6 +22,12 @@
     #   nssmdns4 = true;
     #   openFirewall = true;
     # };
+    activemq = {
+      enable = true;
+      configurationDir = "/home/marts/.nixos/nixos/activemq";
+      baseDir = "/var/lib/activemq";
+      javaProperties."org.apache.activemq.SERIALIZABLE_PACKAGES" = "java.lang";
+    };
   };
 
   programs = {
