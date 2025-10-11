bdf050f7d632429ee869204a5466be650f69eede
weriomat <engel@weriomat.com>
2025-10-11 23:43:34 +0200

    chore: cleanup

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,6 +7,4 @@
   vikunja-desktop = pkgs.callPackage ./vikunja-desktop { };
   languagetool-ngram-ende = pkgs.callPackage ./languagetool-ngrams { };
   languagetool-ff-model = pkgs.callPackage ./languagetool-ff-model { };
-  # TODO: signal desktop
-  # https://github.com/gvolpe/nix-config/blob/b9ff455faaf5a4890985305e5c7a5a01606d20f3/home/modules/signal.nix
 }
