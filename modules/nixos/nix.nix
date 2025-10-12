4739be6ae8513ebe9ed1aae0fc31cf5442619df1
weriomat <engel@weriomat.com>
2025-10-12 22:41:42 +0200

    chore: add nix gaming cachix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -45,18 +45,20 @@ in
         warn-dirty = false;
         use-xdg-base-directories = true;
         substituters = [
-          "https://hyprland.cachix.org"
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
+          "https://hyprland.cachix.org"
           "https://helix.cachix.org"
           "https://devenv.cachix.org/"
+          "https://nix-gaming.cachix.org"
         ];
         trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         ];
       };
 
