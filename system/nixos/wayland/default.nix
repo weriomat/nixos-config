9ab27a1ad2da2a9638cda792f5d196157dba9042
weriomat <engel@weriomat.com>
2025-10-12 22:29:06 +0200

    chore: add hyprpolkitagent

--- system/nixos/wayland/default.nix
+++ system/nixos/wayland/default.nix
@@ -84,6 +84,19 @@ in
           ConditionUser = "!@system";
         };
       };
+      # services.hyprpolkitagent = {
+      #   description = "Hyprpolkitagent - Polkit authentication agent";
+      #   wantedBy = [ "graphical-session.target" ];
+      #   wants = [ "graphical-session.target" ];
+      #   after = [ "graphical-session.target" ];
+      #   serviceConfig = {
+      #     Type = "simple";
+      #     ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
+      #     Restart = "on-failure";
+      #     RestartSec = 1;
+      #     TimeoutStopSec = 10;
+      #   };
+      # };
 
       # Set default hyprland environment
       extraConfig = ''
