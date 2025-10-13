4bfece18b526fff1dad070bc3f328ca94c0971f7
weriomat <engel@weriomat.com>
2025-10-13 14:42:37 +0200

    chore: factor out jq

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -56,20 +56,6 @@
   };
 
   programs = {
-    jq = {
-      enable = true;
-      colors = {
-        null = "1;30";
-        false = "0;31";
-        true = "0;32";
-        numbers = "0;36";
-        strings = "0;33";
-        arrays = "1;35";
-        objects = "1;35";
-        objectKeys = "1;37";
-      };
-    };
-
     tealdeer = {
       enable = true; # tldr written in rust
       settings = {
--- home/shared/terminal/cli/default.nix
+++ home/shared/terminal/cli/default.nix
@@ -6,6 +6,7 @@
     ./eza
     ./fastfetch
     ./fzf
+    ./jq
     ./manix
     ./nh
     ./nix-index
