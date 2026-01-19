c6e44f8a0936f2be7bc3a68895b628bcce81ee68
weriomat <engel@weriomat.com>
2026-01-19 21:57:06 +0100

    chore: switch to swayosd
    We have graphical stuff for util keys

--- modules/home-manager/apps/audio_config/default.nix
+++ modules/home-manager/apps/audio_config/default.nix
@@ -11,51 +11,8 @@ let
     mkIf
     mkMerge
     getExe
+    getExe'
     ;
-  toggle-mic = pkgs.writeShellApplication {
-    name = "toggle-mic";
-    runtimeInputs = [
-      pkgs.pamixer
-      pkgs.libnotify
-      pkgs.gnugrep
-      pkgs.gawk
-      pkgs.coreutils
-    ];
-
-    text = ''
-      #!/usr/bin/env bash
-      if ! command -v pamixer > /dev/null; then
-          exit_code="$?"
-
-          if [ "$notify" ]; then
-              notify-send \
-                  --hint=int:transient:1 \
-                  "Error" \
-                  "'pamixer' is not available."
-          fi
-
-          exit $exit_code
-      fi
-
-      pamixer --list-sources \
-          | grep input \
-          | awk '{print $1}' \
-          | while read -r index; do
-          mic_muted="$(pamixer --source "$index" --get-mute)"
-
-          word=$(pamixer --list-sources | grep input | grep "$index" | awk '{print $4}')
-          line=$(pamixer --list-sources | grep input | grep "$index" | grep -o "''${word}[^\"]*" | grep -o "[^\"]*")
-          if [ "$mic_muted" = "true" ]; then
-              pamixer --source "$index" -u
-              notify-send --hint=int:transient:1 "$line Umuted"
-          else
-              pamixer --source "$index" -m
-              notify-send --hint=int:transient:1 "$line Muted"
-          fi
-
-      done
-    '';
-  };
 in
 {
   options.audio_config.enable = mkEnableOption "HomeManager audio setup";
@@ -76,6 +33,8 @@ in
     # services.pasystray = {};
     # services.easyeffects = {};
 
+    services.swayosd.enable = true;
+
     wayland.windowManager.hyprland.settings = {
       exec-once = [
         "${getExe pkgs.sway-audio-idle-inhibit} &"
@@ -85,23 +44,22 @@ in
       # NOTE: the [e] means repeating
       binde = mkMerge [
         [
-          ",XF86AudioRaiseVolume, exec, ${getExe pkgs.pamixer} -i 2"
-          ",XF86AudioLowerVolume, exec, ${getExe pkgs.pamixer} -d 2"
-          ",XF86AudioNext, exec, ${getExe pkgs.playerctl} next"
-          ",XF86AudioPrev, exec, ${getExe pkgs.playerctl} previous"
+          ",XF86AudioRaiseVolume, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --output-volume raise"
+          ",XF86AudioLowerVolume, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --output-volume lower"
+          ",XF86AudioNext, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --playerctl next"
+          ",XF86AudioPrev, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --playerctl previous"
         ]
         (mkIf globals.laptop [
-          ",XF86MonBrightnessUp, exec, ${getExe pkgs.brightnessctl} set +5%" # TODO: https://github.com/ErikReider/SwayOSD + https://github.com/basecamp/omarchy/blob/f5cd7a4237ca4aad549a6225c472e97d58a825dd/default/hypr/bindings/media.conf
-          ",XF86MonBrightnessDown, exec, ${getExe pkgs.brightnessctl} set 5%-"
-
+          ",XF86MonBrightnessUp, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --brightness raise"
+          ",XF86MonBrightnessDown, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --brightness lower"
         ])
       ];
 
       bind = [
-        ",XF86AudioMute, exec, ${getExe pkgs.pamixer} -t"
-        ",XF86AudioMicMute, exec, ${getExe toggle-mic}"
-        ",XF86AudioPlay, exec, ${getExe pkgs.playerctl} play-pause"
-        ",XF86AudioStop, exec, ${getExe pkgs.playerctl} stop"
+        ",XF86AudioMute, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --output-volume mute-toggle"
+        ",XF86AudioMicMute, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --input-volume mute-toggle"
+        ",XF86AudioPlay, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --playerctl play-pause"
+        ",XF86AudioStop, exec, ${getExe' config.services.swayosd.package "swayosd-client"} --playerctl stop"
       ];
 
       # TODO: maybe this in per host config as well? `hyprctl clients`
