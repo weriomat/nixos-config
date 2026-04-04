a0c7979f10debf6895bcb53f72fcdfc00f2d3a8b
weriomat <engel@weriomat.com>
2026-04-04 12:39:07 +0200

    fix: warning about pkgs.system beeing removed

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,10 @@
-# This file defines overlays
 { inputs, ... }:
 {
-  # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      inherit (final) system;
+      inherit (final.stdenv.hostPlatform) system;
       config.allowUnfree = true;
     };
   };
