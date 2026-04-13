9cb3634fe02674227eef3d13b4d4c59dcfe79798
weriomat <engel@weriomat.com>
2026-04-13 13:30:44 +0200

    chore: format

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 rec {
-  catppuccin-supersonic = pkgs.callPackage ./catppuccin-supersonic { };
-  waybar-yubikey = pkgs.callPackage ./waybar-yubikey { };
-  usbguard-dbus = pkgs.callPackage ./usbguard-dbus { };
-  waybar-usbguard-wrapped = pkgs.callPackage ./waybar-usbguard-wrapped { inherit usbguard-dbus; };
-  weriomat-wallpapers = pkgs.callPackage ./weriomat-wallpapers { };
   apple-color-emoji = pkgs.callPackage ./apple-color-emoji { };
+  catppuccin-supersonic = pkgs.callPackage ./catppuccin-supersonic { };
   cf-terraforming = pkgs.callPackage ./cf-terraforming { };
-  wl-ocr = pkgs.callPackage ./wl-ocr { };
-  languagetool-ngram-ende = pkgs.callPackage ./languagetool-ngrams { };
   languagetool-ff-model = pkgs.callPackage ./languagetool-ff-model { };
+  languagetool-ngram-ende = pkgs.callPackage ./languagetool-ngrams { };
+  usbguard-dbus = pkgs.callPackage ./usbguard-dbus { };
   vja = pkgs.callPackage ./vja { };
+  waybar-usbguard-wrapped = pkgs.callPackage ./waybar-usbguard-wrapped { inherit usbguard-dbus; };
+  waybar-yubikey = pkgs.callPackage ./waybar-yubikey { };
+  weriomat-wallpapers = pkgs.callPackage ./weriomat-wallpapers { };
+  wl-ocr = pkgs.callPackage ./wl-ocr { };
 }
