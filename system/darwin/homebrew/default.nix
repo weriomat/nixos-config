dd2ff2091f674238f97e80831524bedde72a1ff6
weriomat <engel@weriomat.com>
2026-05-15 22:09:13 +0200

    chore: bump homebrew set
    I have reinstalled homebrew to be under /opt/homebrew instead of
    /usr/local/homebrew. Cleanup a few packages...

--- system/darwin/homebrew/default.nix
+++ system/darwin/homebrew/default.nix
@@ -1,8 +1,18 @@
-_: {
+{ config, ... }:
+let
+  cfg = config.homebrew;
+in
+{
+  # FIXME: when 26.05 lands this will be deprecated
+  programs.zsh.interactiveShellInit = ''
+    eval "$(${cfg.brewPrefix}/brew shellenv zsh)"
+  '';
+
+  # We install Vorta/ Borgbackup with proper fuse support, see https://github.com/borgbackup/homebrew-tap and https://vorta.borgbase.com/install/macos/
   homebrew = {
     enable = true;
     onActivation = {
-      autoUpdate = false; # Don't update during rebuild
+      autoUpdate = true; # update during rebuild
       cleanup = "zap"; # Uninstall all programs not declared
       upgrade = true;
     };
@@ -10,48 +20,11 @@ _: {
       brewfile = true; # Run brew bundle from anywhere
       lockfiles = false; # Don't save lockfile (since running from anywhere)
     };
-    taps = [
-      # "homebrew/cask" # Required for casks
-      "homebrew/cask-drivers" # Used for Logitech G-Hub
-      "homebrew/bundle"
-      "homebrew/services"
-      "osx-cross/arm"
-      "osx-cross/avr"
-      "qmk/qmk"
-      "borgbackup/tap"
-    ];
-    brews = [
-      "trash" # Delete files and folders to trash instead of rm
-      "openjdk" # Required by Apache Directory Studio
-      "bootloadhid"
-      "hopenpgp-tools"
-      "openjdk@17"
-      "osx-cross/arm/arm-none-eabi-gcc@8"
-      "osx-cross/avr/avr-gcc@8"
-      "pcre"
-      "pillow"
-      "pinentry-mac"
-      "qmk/qmk/hid_bootloader_cli"
-      "qmk/qmk/mdloader"
-      "qmk/qmk/qmk"
-      "avrdude"
-      "screenresolution"
-      "borgbackup/tap/borgbackup-fuse"
-      "apache-activemq"
-    ];
+    taps = [ "borgbackup/tap" ];
+    brews = [ "borgbackup/tap/borgbackup-fuse" ];
     casks = [
-      # "scroll-reverser" # Different scroll style for mouse vs. trackpad
-      # "logitech-g-hub" # Mouse and keyboard management
-      # "obsidian" # Obsidian packaging on Nix is not available for macOS
-      # "steam" # Not packaged for Nix
-      # "apache-directory-studio" # Packaging on Nix is not available for macOS
-      "balenaetcher"
-      "mqtt-explorer"
-      "qmk-toolbox"
-      "wireshark-chmodbpf"
-      # "vorta"
+      "vorta" # TODO: package or use with brew
       "macfuse"
-      # "qmk-toolbox"
     ];
   };
 }
