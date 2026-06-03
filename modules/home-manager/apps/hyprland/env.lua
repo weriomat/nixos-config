a65462f93bb02cadf304fc932fee25e351df2343
weriomat <engel@weriomat.com>
2026-06-03 15:28:15 +0200

    chore: switch to qt6 by default

--- modules/home-manager/apps/hyprland/env.lua
+++ modules/home-manager/apps/hyprland/env.lua
@@ -8,7 +8,7 @@ hl.env("SDL_VIDEODRIVER", "wayland")
 
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
--- hl.env("QT_QPA_PLATFORMTHEME","qt5ct")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_ENABLE_HIGHDPI_SCALING", "1")
 hl.env("WLR_RENDERER_ALLOW_SOFTWARE", "1")
