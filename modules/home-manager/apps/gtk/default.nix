ea5b2c0d6bd1572963f635de02d16ce2cb68e9e8
weriomat <engel@weriomat.com>
2026-06-03 16:18:45 +0200

    chore: bump gtk settings

--- modules/home-manager/apps/gtk/default.nix
+++ modules/home-manager/apps/gtk/default.nix
@@ -13,49 +13,32 @@ in
   # TODO: theme gtk
   config = mkIf config.my_gtk.enable {
     # TODO: https://github.com/Sly-Harvey/NixOS/blob/master/modules/themes/Catppuccin/default.nix
-    # TODO: https://github.com/different-name/nix-files/blob/master/modules/home/catppuccin-gtk.nix
-    # catppuccin.gtk = {
-    #   enable = true;
-    #   flavor = "mocha";
-    #   size = "compact";
-    #   accent = "lavender";
-    #   gnomeShellTheme = true;
-    #   icon = {
-    #     enable = true;
-    #     flavor = "mocha";
-    #     accent = "lavender";
-    #   };
-    # };
-    # TODO: https://github.com/camillemndn/infra/blob/main/profiles/home-manager/gtk/default.nix
+
     gtk = {
       enable = true;
-      gtk3.extraConfig.Settings = "gtk-application-prefer-dark-theme=1 ";
-      gtk4.extraConfig.Settings = "gtk-application-prefer-dark-theme=1 ";
-      # Alternative     name = "BeautyLine"; package = pkgs.beauty-line-icon-theme;
-      # iconTheme = {
-      #   name = "Papirus-Dark";
-      #   package = pkgs.catppuccin-papirus-folders.override {
-      #     flavor = "mocha";
-      #     accent = "lavender";
-      #   };
-      # };
-      # Alternative     name = "Juno-ocean"; package = pkgs.juno-theme;
-      # theme = {
-      #   name = "catppuccin-mocha-lavender-compact";
-      #   # name = "catppuccin-mocha-Compact-Lavender-Dark";
-      #   package = pkgs.catppuccin-gtk.override {
-      #     accents = [ "lavender" ];
-      #     size = "compact";
-      #     # tweaks = [ "rimless" ];
-      #     variant = "mocha";
-      #   };
-      # };
+
+      gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+      gtk3.extraConfig."gtk-application-prefer-dark-theme" = "1";
+      gtk4 = {
+        extraConfig."gtk-application-prefer-dark-theme" = "1";
+        theme = config.gtk.theme;
+      };
+
+      theme = {
+        name = "catppuccin-mocha-lavender-compact";
+        package = pkgs.catppuccin-gtk.override {
+          accents = [ "lavender" ];
+          size = "compact";
+          variant = "mocha";
+        };
+      };
+
       cursorTheme = {
         name = "Nordzy-cursors";
         package = pkgs.nordzy-cursor-theme;
         size = 22;
       };
-      gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
       #      # the dark files are not copied by default, as not all themes have separate files
       #      # see: https://github.com/nix-community/home-manager/blob/afcedcf2c8e424d0465e823cf833eb3adebe1db7/modules/misc/gtk.nix#L238
       #      extraCss = ''
@@ -64,12 +47,30 @@ in
       #    };
     };
 
-    # home.sessionVariables.GTK_THEME = "adw-gtk3";
-
     home.pointerCursor = {
       name = "Nordzy-cursors";
       package = pkgs.nordzy-cursor-theme;
       size = 22;
+      gtk.enable = true;
+      x11.enable = true;
+    };
+
+    # FROM: https://github.com/Sly-Harvey/NixOS/blob/9c81fadf81f9d53624b882fcfb7cbcf16181b346/modules/themes/Catppuccin/default.nix
+    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+    xdg.configFile = {
+      "gtk-4.0/assets" = {
+        force = true;
+        source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+      };
+      "gtk-4.0/gtk.css" = {
+        force = true;
+        source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+      };
+      "gtk-4.0/gtk-dark.css" = {
+        force = true;
+        source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+      };
     };
   };
 }
