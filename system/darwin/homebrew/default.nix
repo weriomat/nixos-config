6af7e4d1ab321f5a0ef06fb3d93a360d44baba5a
weriomat <engel@weriomat.com>
2026-06-11 14:50:40 +0200

    chore: bump homebrew to 26.05 config

--- system/darwin/homebrew/default.nix
+++ system/darwin/homebrew/default.nix
@@ -1,16 +1,9 @@
-{ config, ... }:
-let
-  cfg = config.homebrew;
-in
+{ ... }:
 {
-  # FIXME: when 26.05 lands this will be deprecated
-  programs.zsh.interactiveShellInit = ''
-    eval "$(${cfg.brewPrefix}/brew shellenv zsh)"
-  '';
-
   # We install Vorta/ Borgbackup with proper fuse support, see https://github.com/borgbackup/homebrew-tap and https://vorta.borgbase.com/install/macos/
   homebrew = {
     enable = true;
+    enableZshIntegration = true;
     onActivation = {
       autoUpdate = true; # update during rebuild
       cleanup = "zap"; # Uninstall all programs not declared
@@ -18,7 +11,6 @@ in
     };
     global = {
       brewfile = true; # Run brew bundle from anywhere
-      lockfiles = false; # Don't save lockfile (since running from anywhere)
     };
     taps = [ "borgbackup/tap" ];
     brews = [ "borgbackup/tap/borgbackup-fuse" ];
